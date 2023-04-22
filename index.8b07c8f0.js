4f520f30c03280ec315ba8d6fe4eb9f5d85bab13
Grzesiek1982 <42001358+Grzesiek1982@users.noreply.github.com>
2023-04-22 09:44:20 +0000

    Deploying to gh-pages from @ Grzesiek1982/test-gf@17b96453db819aae204814ee96dafbf038d976bb 🚀

---
+++ index.8b07c8f0.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelectorAll("[data-modal-open]"),closeModalBtn:document.querySelectorAll("[data-modal-close]"),modal:document.querySelectorAll("[data-modal]")};function l(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",l),e.closeModalBtn.addEventListener("click",l)})();
+//# sourceMappingURL=index.8b07c8f0.js.map
